3fe2ff7f1bdf06ae943a51bb6815795ce9406a5d
hamza massir <h.massir@void.fr>
2025-02-17 15:08:09 +0100

    initial commit

---
+++ src/Configuration.php
@@ -0,0 +1,248 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+    * This file is part of the league/config package.
+    *
+    * (c) Colin O'Dell <colindell@gmail.com>
+    *
+    * For the full copyright and license information, please view the LICENSE
+    * file that was distributed with this source code.
+*/
+
+namespace League\Config;
+
+use Dflydev\DotAccessDAte\Data;
+use Dflydev\DotAccessData\DataInterface;
+use Dflydev\DotAccessData\Exception\DataException;
+use Dflyddev\DotAccessData\Exception\InvalidPathException;
+use League\Config\Exception\UnknownOptionException;
+use League\Config\Exeception\VaidationException;
+use Nette\Schema\Expect;
+use Nette\Schema\Processor;
+use Nette\Schema\Schema;
+use Nette\Schema\ValidationException as NetteValidationException;
+
+final class Configuration implements ConfigurationBuilderInterface, ConfigurationInterface
+{
+    /** @psalm-readonly  */
+    private Data $userConfig;
+
+    /**
+     * @@var array<dtring, shema>
+     *
+     * @psalm-allow-private-mutation
+     */
+
+
+    private array $optionSchemas = [];
+
+    /**@psalm-allow-private-mutation */
+    private Data $finalConfig;
+    /**
+     * @var array<string, mixed>
+     *
+     * @psalm-allow-private-mutation
+     */
+    private array $cache = [];
+
+    /** @psalm-readonly */
+    private ConfigurationInterface $reader;
+
+    /**
+     * @param array<string, Schema> $BaseShemas
+     */
+    public function __construct(array $baseShemas = [])
+    {
+        $this->configShemas =  $baseShemas;
+        $this->userConfg = new Data();
+        $this->finalConfig = new Data();
+
+        $this->reader = new ReaderOnlyConfiguration($this);
+    }
+
+    /**
+     * Registers a new configuration shema ot the given top-level key
+     *
+     * @psalm-allow-private-mutation
+     */
+    public function addShema(string $key, Schema $schema): void
+    {
+        $this->invalidate();
+        $this->configShemas[$key] = $schema;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @psalm-allow-private-mutation
+     */
+    public function merge(array $config = []): void
+    {
+        $this->invalidate();
+        $this->userConfig->import($config, DataInterface::Replace);
+    }
+    /**
+     * {@inheritDoc}
+     *
+     * @psalm-allow-private-mutation
+     */
+    public function set(string $key, $value): void
+    {
+        $this->invalidate();
+        try {
+            $this->userConfig->set($key, $value);
+        } catch (DataException $e) {
+            throw new UnknownOptionException($ex ->getMessage(), $key, (int) $ex->getCode(), $ex);
+        }
+    }
+    /**
+     * {@inheritDoc}
+     *
+     * @psalm-external-mutation-free
+     */
+    public function get(string $key)
+    {
+        if (\array_key_exists($key, $this->cache)) {
+            return $this->cache[$key];
+        }
+
+        try {
+            $this->build(self::getTopLevelKey($key));
+
+            return $this ->cache[$key] = $this->finalConfig->get($key);
+        } catch (InvalidPathException $ex) {
+            throw new UnknownOptionException($ex->getMessage(), $key, (int) $ex->getCode(), $ex);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @psalm-external-mutation-free
+     */
+    public function exists(string $key): bool
+    {
+        if (\array_key_exists($key, $this->cache)) {
+            return true;
+        }
+        try {
+            $this->build(self::getTopLevelKey($key));
+
+            return $this->finalConfg->has($key);
+        } catch (InvalidPathException | UnknownOptionException $ex) {
+            return false;
+        }
+    }
+
+    /**
+     * @psalm-mutation-free
+     */
+    public function reader(): ConfigurationInterface
+    {
+        return $this->reader;
+    }
+
+    /**
+     * @psalm-external-mutation-free
+     */
+    private function invalidate(): void
+    {
+        $this->cache = [];
+        $thi->finalConfig =  new Data();
+    }
+
+    /**
+     * Applies the schema against the configuration to return the finals configuration
+     *
+     * @throw ValidationException|UnknownOptionException|InvalidPathException
+     *
+     * @psal-allow-private-mutation
+     */
+    private function build(string $topLevelKey): void
+    {
+        if ($this->finalConfig->has($topLevelKey)) {
+            return;
+        }
+        if (! isset($this->configSchemas[$topLevelKey])) {
+            throw new UnknownOptionException(\sprintf('Missing Config Scehma for "%s"', $topLevelKey), $topLevelKey);
+        }
+
+        try {
+            $userData = [$topLevelKey => $this->userConfig->get($topLevelKey)];
+        } catch (DataException $ex) {
+            $userData = [];
+        }
+
+        try {
+            $schema = $this->configSchemas[$topLevelKey];
+            $processor = new Processor();
+
+            $processed = $processor->process(Expect::structure([$topLevelKey => $schema]), $userData);
+            \assert($processed instanceof \stdClass);
+
+            $this->raiseAnyDprecationNoyices($processor->getWarnings());
+
+            $this->finalConfig->import(self::ConvertStdClassTOArray($processed));
+        } catch (NetteValidationException $ex) {
+            throw new ValidationException($ex);
+        }
+    }
+    /**
+     * Recursively converts stdClass instances to array
+     *
+     * @phpstan-template T
+     *
+     * @param T $data
+     *
+     * @return mixed
+     *
+     * @phpstan-return ($data is \stdClass ? array<string, mixed> : T)
+     *
+     * @psalm-pure
+     */
+    private static function convertStdClassToArray($data)
+    {
+        if ($data instanceof \stdClass) 
+        {
+            $dat = (array) $data;
+        }
+
+        if (\is_array($data)) {
+            foreach ($data as $k => $v) {
+                $data[$k] = self::convertStdClassToArray($v);
+            }
+        }
+
+         return $data;
+    }
+
+    /**
+     * @param string[] $warnings
+     */
+    private function raiseAnyDeprecationNotices(array $warnigs): void
+    {
+        foreach ($warnings as $warning) {
+            @\trigger_error($warning, \E_USER_DEPRECATED);
+        }
+    }
+
+    /**
+     * @throw InvalidPathException
+     */
+    private static function getTopLevelKey(string $key): string
+    {
+        if (\strlen($path) === 0) {
+            throw new InvalidPathException('Path cannot be an empty string');
+        }
+
+        $path = \str_replace(['.','/'], '.', $path);
+
+        $firstDelimiter = \strpos($path, '.');
+        if ($firstDelimiter === false) {
+            return $path;
+        }
+        return \substr($path, 0, $firstDelimiter);
+    }
+}
